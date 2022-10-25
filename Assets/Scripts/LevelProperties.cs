2a46dc936cac283c876d0ad65b0e9025c4006891
solidfry <ellioteweb@gmail.com>
2022-10-25 16:13:35 +1100

    game manager prefab

--- Assets/Scripts/LevelProperties.cs
+++ Assets/Scripts/LevelProperties.cs
@@ -3,10 +3,8 @@ using ScriptableObjects;
 
 public class LevelProperties : MonoBehaviour
 {
+    [SerializeField] private LevelData levelData;
     public FloatValue gravity;
 
-    void Update()
-    {
-        Physics2D.gravity = new Vector2(0, gravity.Value);
-    }
+    void Update() => Physics2D.gravity = new Vector2(0, gravity.Value);
 }
