7b88d06743aff20567464be5c5264461d8def2b8
solidfry <ellioteweb@gmail.com>
2022-10-25 15:57:01 +1100

    global rotation on fan

--- Assets/SetRotationGlobal.cs
+++ Assets/SetRotationGlobal.cs
@@ -3,12 +3,7 @@ using UnityEngine;
 
 public class SetRotationGlobal : MonoBehaviour
 {
-    void Start()
-    {
-        var t = this.transform;
-        var acos = Mathf.Acos(t.rotation.z * t.parent.rotation.z);
-        var acosToDeg = acos * Mathf.Rad2Deg;
-        print(acos);
-        transform.Rotate(Vector3.forward, acosToDeg ,Space.World);
-    }
+    void Start() => transform.rotation = Quaternion.Euler(0,0,0);
+
+    private void OnValidate() => transform.rotation = Quaternion.Euler(0,0,0);
 }
